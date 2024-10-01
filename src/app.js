51f78eee98f9390a3c8b1875ac0b64ca45bdc04a
Rohan sahu <therohansahu7@gmail.com>
2024-10-01 10:19:21 +0530

    CORS new configuration

--- src/app.js
+++ src/app.js
@@ -3,8 +3,20 @@ import cors from "cors";
 
 const app = express();
 
+const allowedOrigins = [
+  'https://story-sharing-sigma.vercel.app',
+  'http://localhost:5173'
+]
+
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: (origin, callback) => {
+        if(!origin) return callback(null, true);
+        if( allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
 }))
 
@@ -17,6 +29,7 @@ app.use(express.static("public"));
 
 import userRouter from "./routes/user.route.js"
 import storyRouter from "./routes/story.route.js"
+import { registerUser } from "./controllers/user.controller.js";
 
 
 // Route Declaration
